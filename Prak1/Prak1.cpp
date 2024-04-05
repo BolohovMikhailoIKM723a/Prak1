fe76aa0f336e767764c3b00527144ce19617f3f7
Михаил <@DESKTOP-1RCLF6R>
2024-04-05 20:52:51 +0300

    Add project files.

---
+++ Prak1/Prak1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int number;
+    int* ptr;   
+    cout << "введіть число: ";
+    cin >> number;
+    ptr = &number; 
+    cout << "значення числа: " << *ptr << endl;
+    *ptr = 100;
+    cout << "нове значення числа: " << *ptr << endl;
+    return 0;
+}
